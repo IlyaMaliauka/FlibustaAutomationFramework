6b4caccca7d7a1b5ea7a60a511d3c5e25bbf7ad3
IlyaMaliauka <molyavko.edu@gmail.com>
2020-10-12 19:30:23 +0300

    add listener

--- src/test/java/com/automation/test/CommonConditions.java
+++ src/test/java/com/automation/test/CommonConditions.java
@@ -1,10 +1,13 @@
 package com.automation.test;
 
 import com.automation.driver.DriverSingleton;
+import com.automation.util.TestListener;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners({TestListener.class})
 public class CommonConditions {
     protected WebDriver driver;
 
